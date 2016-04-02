0bc26d490645a1baa27110fdc00607a7cc2d6520
shenyou <shenyou@qsy>
2016-04-02 08:33:00 +0800

    s

--- WebContent/resources/js/main.js
+++ WebContent/resources/js/main.js
@@ -1,13 +1,17 @@
 $(function(){
-	 $("#side-menu").metisMenu({ doubleTapToGo: true});
 	 $("#menu").metisMenu();
 	 $("#nav-menu").slimScroll({height:"auto",color:"#656565",railOpacity:0.9,wheelStep:10,alwaysVisible: true});
-	 $(".sidebar-collapse").slimScroll({height:"100%",railOpacity:.4,wheelStep:10});
-	 $(".navbar-minimalize").click(function(){$("body").toggleClass("mini-navbar"),SmoothlyMenu()});
+	 $("#myTab .fa").bind("click",function(event){
+		 
+		 event.stopPropagation(); //  阻止事件冒泡
+	 });
+	 $("#myTab .fa").bind("mouseover",function(event){
+		 this.style.color="red";
+	 });
+	 $("#myTab .fa").bind("mouseout",function(event){
+		 this.style.color="";
+	 });
 });
-function SmoothlyMenu(){
-	$("body").hasClass("mini-navbar")?$("body").hasClass("fixed-sidebar")?($("#side-menu").hide(),setTimeout(function(){$("#side-menu").fadeIn(500)},300)):$("#side-menu").removeAttr("style"):($("#side-menu").hide(),setTimeout(function(){$("#side-menu").fadeIn(500)},100));
-}
 
 function displaynavbar(obj){
 	if($(obj).hasClass("open")){
@@ -16,6 +20,5 @@ function displaynavbar(obj){
 	}else{
 		$(obj).addClass("open");
 		$("body").addClass("big-page");
-					
 	}
 }
